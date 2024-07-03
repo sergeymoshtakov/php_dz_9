525ed7eb3d9ee04bf5882af3415aff5cad0e210a
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-07-03 12:53:10 +0200

    added readme

--- fetch_table_names.php
+++ fetch_table_names.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "root";
-$password = "Moshtakov2005s";
+$password = "";
 $dbname = "dz_nine_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
